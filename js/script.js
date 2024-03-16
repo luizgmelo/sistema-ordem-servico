f5877535101409dd5d6e38e13bbce0c020a509a4
Luiz Guilherme <88911920+luizgmelo@users.noreply.github.com>
2024-03-16 18:53:14 -0300

    feat: add validate Client Name

--- js/script.js
+++ js/script.js
@@ -11,17 +11,34 @@ submitButton.addEventListener("click", () => {
   const isPoweringUp = document.getElementById('isPoweringUp');
   const problem = document.getElementById('problem');
   const techNote = document.getElementById('techNote');
+  
+  const saveDataAndRedirect = () => {
+    localStorage.setItem('name', clientName.value);
+    localStorage.setItem('phone', phoneNumber.value);
+    localStorage.setItem('isWhatsApp', isWhatsApp.checked);
+    localStorage.setItem('date', requestDate.value);
+    localStorage.setItem('brand', brand.value);
+    localStorage.setItem('hasCharger', hasCharger.checked);
+    localStorage.setItem('isPoweringUp', isPoweringUp.checked);
+    localStorage.setItem('problem', problem.value);
+    localStorage.setItem('techNote', techNote.value);
+    location.replace("../pdf.html");
+  }
 
-  localStorage.setItem('name', clientName.value);
-  localStorage.setItem('phone', phoneNumber.value);
-  localStorage.setItem('isWhatsApp', isWhatsApp.checked);
-  localStorage.setItem('date', requestDate.value);
-  localStorage.setItem('brand', brand.value);
-  localStorage.setItem('hasCharger', hasCharger.checked);
-  localStorage.setItem('isPoweringUp', isPoweringUp.checked);
-  localStorage.setItem('problem', problem.value);
-  localStorage.setItem('techNote', techNote.value);
-  location.replace("../pdf.html");
+  const validateClientName = (name) => {
+    const pattern = /^[a-zA-Z\s]+$/;
+    if (!name) {
+      alert("Por favor digite o nome do cliente.");
+    }
+    else if (!pattern.test(name)) {
+      alert("Nome do cliente é inválido, Por favor verifique o nome do cliente");
+    }
+    else {
+      saveDataAndRedirect()
+    }
+  }
+  // <TO-DO Validar Celular, Data do pedido, marca, defeito e anotação técnica>
+  validateClientName(clientName.value)
 });
 
 
