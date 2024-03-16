8e0c90e99fe7845d924ef6a843b34f657fd5b800
Luiz Guilherme <88911920+luizgmelo@users.noreply.github.com>
2024-03-16 08:50:07 -0300

    feat: send and get data from index to pdf html with localStorage

---
+++ js/pdf.js
@@ -0,0 +1,11 @@
+const clientName = localStorage.getItem('name');
+const phoneNumber = localStorage.getItem('phone');
+const isWhatsApp = localStorage.getItem('isWhatsApp')
+const requestDate = localStorage.getItem('date');
+const brand = localStorage.getItem('brand');
+const hasCharger = localStorage.getItem('hasCharger')
+const isPoweringUp = localStorage.getItem('isPoweringUp')
+const problem = localStorage.getItem('problem');
+const techNote = localStorage.getItem('techNote');
+
+
--- js/script.js
+++ js/script.js
@@ -1,6 +1,26 @@
 const submitButton = document.getElementById("submitButton");
 
+
 submitButton.addEventListener("click", () => {
+  const clientName = document.getElementById('clientName');
+  const phoneNumber = document.getElementById('phoneNumber');
+  const isWhatsApp = document.getElementById('checkWhatsApp');
+  const requestDate = document.getElementById('requestDate');
+  const brand = document.getElementById('brand');
+  const hasCharger = document.getElementById('hasCharger');
+  const isPoweringUp = document.getElementById('isPoweringUp');
+  const problem = document.getElementById('problem');
+  const techNote = document.getElementById('techNote');
+
+  localStorage.setItem('name', clientName.value);
+  localStorage.setItem('phone', phoneNumber.value);
+  localStorage.setItem('isWhatsApp', isWhatsApp.value);
+  localStorage.setItem('date', requestDate.value);
+  localStorage.setItem('brand', brand.value);
+  localStorage.setItem('hasCharger', hasCharger.value);
+  localStorage.setItem('isPoweringUp', isPoweringUp.value);
+  localStorage.setItem('problem', problem.value);
+  localStorage.setItem('techNote', techNote.value);
   location.replace("../pdf.html");
 });
 
